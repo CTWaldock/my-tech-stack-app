9197d6b0b9b74c0f337fdf865066310fdbcb66ae
Clayton-ty <c.t.waldock@outlook.com>
2018-06-14 08:55:54 +1000

    added common components and placed header in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import react from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './reducers';
+import reducers from './Reducers';
+import Header from './Components/Common/Header';
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <Header headerText="My Tech Stack"/>
+      </View>
     </Provider>
   );
 };
